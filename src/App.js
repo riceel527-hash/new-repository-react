a6722e8be18de056d720ca139cd2a10c9263526f
riceel527-hash <riceel527@gmail.com>
2026-07-15 09:07:55 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Discounted from './Components/Discounted';
+import Featured from './Components/Featured';
+import Highlights from './Components/Highlights';
+import Nav from './Components/Nav';
+import Landing from './Components/Landing';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
+      <Discounted />
     </div>
   );
 }
 
 export default App;
+
+
+
