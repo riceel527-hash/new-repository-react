73cf3ce98d97dfc2b78135f9920e297afab9a865
riceel527-hash <riceel527@gmail.com>
2026-07-21 15:16:43 -0400

    added css

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -20,22 +20,39 @@ function BookInfo({ books }) {
                         </div>
                         <div className="book__selected">
                             <figure className="book__selected--figure">
-                                <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--ig" />
+                                <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" 
+                                className="book__selected--ig" />
                             </figure>
                             <div className="book__selected--description">
-                                <h2 className="book__selected--title">Crack the coding interview</h2>
+                                <h2 className="book__selected--title">
+                                Crack the coding interview
+                                </h2>
                                 <Rating rating="4.5" />
                                 <div className="book__selected--price">
                                     <Price originalPrice={50} salePrice={20} />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                             </div>
+                                <div className="book__summary">
+                                <div className="book__summary--title">Summary</div> 
+                                <p className="book__summary__para">
+                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam assumenda at cumque! Nam praesentium magni fuga omnis! Dicta vel
+                                  perspiciatis eaque inventore, iste quaerat ea beatae eligendi tempore culpa officiis!
+                               </p>
+                              <p className="book__summary__para">
+                                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam assumenda at cumque! Nam praesentium magni fuga omnis! Dicta vel
+                                  perspiciatis eaque inventore, iste quaerat ea beatae eligendi tempore culpa officiis!
+                               </p>
+                           </div>
+                           <button className="btn">
+                            Add to cart
+                           </button>
+                         </div>
+                       </div>
+                     </div>
+                   </div>
+                  </main>
                 </div>
-            </main>
-        </div>
-    );
-}
+            );
+         };
 
 export default BookInfo;
 
