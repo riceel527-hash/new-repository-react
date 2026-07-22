cbeb58e6cf1c664f77fc4b322992a825f66094ca
riceel527-hash <riceel527@gmail.com>
2026-07-22 12:24:43 -0400

    added css

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,10 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Link } from  'react-router-dom';
+import { Link,useParams } from  'react-router-dom';
 import Rating from "../Components/ui/Rating";
 import Price from "../Components/Price";
 
 function BookInfo({ books }) {
+    const { id } = useParams();
+    const book= books.find(book => +book.id === +id);
+
     return (
         <div id="books__body">
             <main id="books__main">
@@ -20,19 +23,23 @@ function BookInfo({ books }) {
                         </div>
                         <div className="book__selected">
                             <figure className="book__selected--figure">
-                                <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" 
+                                <img 
+                                src={book.url}
+                                alt="" 
                                 className="book__selected--ig" />
                             </figure>
                             <div className="book__selected--description">
                                 <h2 className="book__selected--title">
-                                Crack the coding interview
+                                {book.title}
                                 </h2>
-                                <Rating rating="4.5" />
+                                <Rating rating={book.rating} />
                                 <div className="book__selected--price">
-                                    <Price originalPrice={50} salePrice={20} />
+                                    <Price originalPrice={book.originalprice} salePrice={book.salePrice} />
                              </div>
                                 <div className="book__summary">
-                                <div className="book__summary--title">Summary</div> 
+                                <div className="book__summary--title">
+                                    Summary
+                                    </div> 
                                 <p className="book__summary__para">
                                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam assumenda at cumque! Nam praesentium magni fuga omnis! Dicta vel
                                   perspiciatis eaque inventore, iste quaerat ea beatae eligendi tempore culpa officiis!
@@ -48,6 +55,16 @@ function BookInfo({ books }) {
                          </div>
                        </div>
                      </div>
+                   </div>
+                   <div className="books__container">
+                    <div className="row">
+                        <div className="book__selected--top">
+                            <h2 className="book__selected--title--top">
+                                Recommended Books
+                            </h2>
+                        </div>
+
+                    </div>
                    </div>
                   </main>
                 </div>
