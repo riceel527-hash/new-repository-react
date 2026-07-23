8e679ec7f83df982c20c35056164a59024eabb6e
riceel527-hash <riceel527@gmail.com>
2026-07-23 09:31:59 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Footer from "./Components/Footer";
 import Nav from "./Components/Nav";
-import {BrowserRouter as Router, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from "./data";
@@ -10,15 +10,16 @@ import Price from "./Components/Price";
 
 function App() {
   return (
-    <Router>
+  <Router>
     <div className="App">
       <Nav />
-      <Route path="/" exact component={Home} />
-      <Route path="/books" exact render={() => <Books books={books} />}/>
-      <Route path="/books/:id" render={() => <BookInfo books={books} />} />
-      <Footer />
-    </div>
-    </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books books={books} />} />
+        <Route path="/books/:id" element={<BookInfo books={books} />} />
+    </Routes>
+    <Footer />
+</Router>
   );
 }
 
