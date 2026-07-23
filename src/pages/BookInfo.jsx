eb5800245f2d3dd67e72086635f01fba0c9788d9
riceel527-hash <riceel527@gmail.com>
2026-07-23 08:41:05 -0400

    added css

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -35,7 +35,7 @@ function BookInfo({ books }) {
                                 </h2>
                                 <Rating rating={book.rating} />
                                 <div className="book__selected--price">
-                                <Price originalPrice={book.originalprice} salePrice={book.salePrice} />
+                                <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
                              </div>
                                 <div className="book__summary">
                                 <h3 className="book__summary--title">Summary</h3>
@@ -58,16 +58,22 @@ function BookInfo({ books }) {
                    <div className="books__container">
                     <div className="row">
                         <div className="book__selected--top">
-                            <h2 className="book__selected--title--top">Recommended Books</h2>
+                         <h2 className="book__selected--title--top">
+                            Recommended Books
+                         </h2>
                         </div>
+                        <div className="books">
                         {books
-                          .filter(book => book.rating === 5 && book.id !== id)
-                          .map(book => <Book book={book} key={book.id} />)
-                        }
+                          .filter((book) => book.rating === 5 && +book.id !== +id)
+                          .slice(0, 4)
+                          .map((book) => (
+                          <Book book={book} key={book.id} />
+                           ))}
                     </div>
                    </div>
-                  </main>
-                </div>
+                 </div>
+                </main>
+             </div>
             );
          };
 
