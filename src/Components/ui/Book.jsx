abb611d52f9cae91e05fbf8d99f598b8aa9ad2e0
riceel527-hash <riceel527@gmail.com>
2026-07-29 15:13:05 -0400

    added css

--- src/Components/ui/Book.jsx
+++ src/Components/ui/Book.jsx
@@ -1,22 +1,35 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import Rating from "./Rating";
 import Price from "../Price";
 
 const Book = ({ book }) => {
-    function imageLoaded() {
-        console.log('imageLoaded')
-    }
+    const [img, setImg] = useState();
+
+    useEffect(() => {
+        const image = new Image();
+        image.src = book.url;
+        image.onload = () => {
+            setTimeout(() => {
+
+            }, 300);
+           setImg(image);
+        }
+    })
+
    return (
     <div className="book">
-     <div className="book__img--skeleton"></div>
-     <div className="skeleton book rating--skeleton"></div>
-     <div className="skeleton book price--skeleton"></div>
-        <Link to={`/books/${book.id}`}>
+        {img ? (
+         <>
+          <Link to={`/books/${book.id}`}>
           <figure className="book__img--wrapper">
-            <img src={book.url} alt="" className="book__img" onLoad={imageLoaded} />
-            </figure> 
+            <img
+            src={img.src}
+            alt=""
+            className="book__img"
+          />
+        </figure> 
         </Link>
         <div className="book__title">
             <Link to={`/books/${book.id}`} className="book__title--link">
@@ -24,10 +37,21 @@ const Book = ({ book }) => {
             </Link>
          </div>
           <Rating rating={book.rating} />
-           <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
-    </div>
-    );
-    };
+           <Price
+            salePrice={book.salePrice}
+            originalPrice={book.originalPrice} 
+           />
+        </>
+      ) : (
+        <><div className="book__img--skeleton"></div>
+    <div className="skeleton book__title--skeleton"></div>
+    <div className="skeleton book__rating--skeleton"></div>
+    <div className="skeleton book__price--skeleton"></div>
+    </>
+      )}
+</div>
+     );
+    }
     
     export default Book;
 
