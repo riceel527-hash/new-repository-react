1e9891613468cb71a4188ecd2c7eab0479403387
riceel527-hash <riceel527@gmail.com>
2026-07-30 08:21:42 -0400

    added css

--- src/Components/ui/Book.jsx
+++ src/Components/ui/Book.jsx
@@ -1,5 +1,5 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from 'react-router-dom';
 import Rating from "./Rating";
 import Price from "../Price";
@@ -7,14 +7,21 @@ import Price from "../Price";
 const Book = ({ book }) => {
     const [img, setImg] = useState();
 
+     const mountedRef = useRef(true);
+
     useEffect(() => {
         const image = new Image();
         image.src = book.url;
         image.onload = () => {
             setTimeout(() => {
-
+                if (mountedRef.current) {
+                  setImg(image);
+                }   
             }, 300);
-           setImg(image);
+        };
+        return () => {
+            // When the component unmounts
+            mountedRef.current = false;
         }
     })
 
