9b8fe15ae1c7a0c5486c5fff90b0d3f5b25301d5
riceel527-hash <riceel527@gmail.com>
2026-07-30 12:39:57 -0400

    Remove unused variables

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React,{useState } from 'react';
+import React from 'react';
 import { Link,useParams } from  "react-router-dom";
 import Rating from "../Components/ui/Rating";
 import Price from "../Components/Price";
