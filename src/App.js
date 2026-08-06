de373ce840de2fad0351029dca4cae27f62a7328
riceel527-hash <riceel527@gmail.com>
2026-08-06 09:26:50 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { books } from "./data";
 import BookInfo from "./pages/BookInfo";
 import Cart from "./pages/Cart";
 import { auth,db } from './firebase/init';
-import { collection, addDoc, getDocs, getDoc, doc } from "firebase/firestore";
+import { collection, addDoc, getDocs, getDoc, doc, query, where } from "firebase/firestore";
 import { createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut,onAuthStateChanged, } from "firebase/auth";
 
 
@@ -18,8 +18,9 @@ function App() {
 
   function createPost() {
     const post = {
-    title: "Land a $100k job",
-    description: "Finish Frontend Simplified",
+    title: "Finish Firebase Section",
+    description: "Do Frontend Simplified",
+    uid: user.uid,
   };
   addDoc(collection(db, "posts"), post)
 }
